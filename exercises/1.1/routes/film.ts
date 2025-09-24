9c5ca963db314b21b6700ccd21a67475a55037ff
HediCaprio <hedi.benkhalifa@student.vinci.be>
2025-09-24 14:08:56 +0200

    new: ex1.1

---
+++ exercises/1.1/routes/film.ts
@@ -0,0 +1,37 @@
+import { Router, Request ,Response } from "express"
+
+export const filmRouter = Router();
+
+const films = [
+  {
+    id: 1,
+    title: "Inception",
+    director: "Christopher Nolan",
+    duration: 148,
+    budget: 160,
+    description: "Un film de science-fiction sur les rêves.",
+    imageUrl: "https://image.tmdb.org/t/p/inception.jpg"
+  },
+  {
+    id: 2,
+    title: "The Matrix",
+    director: "Lana et Lilly Wachowski",
+    duration: 136,
+    budget: 63,
+    description: "La matrice est partout.",
+    imageUrl: "https://image.tmdb.org/t/p/matrix.jpg"
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    director: "Christopher Nolan",
+    duration: 169,
+    budget: 165,
+    description: "Voyage à travers un trou de ver.",
+    imageUrl: "https://image.tmdb.org/t/p/interstellar.jpg"
+  }
+];
+
+filmRouter.get("/", (_req:Request ,res:Response) => {
+  res.json(films);
+});
